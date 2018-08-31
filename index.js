c52d763a5e08ac605a42a9398e71b611c231e7d6
Bestun Gardi <bestun95@yahoo.com>
2018-08-31 08:17:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution in this file!
 let driver = {};
 
-updateDriverWithKeyAndValue(driver,key, value){
-  return driver[key] = value;
+updateDriverWithKeyAndValue(drive,key, value){
+   drive[key] = value;
 }
