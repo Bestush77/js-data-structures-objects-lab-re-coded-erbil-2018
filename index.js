b0f18bbc690339d3c154a5d52f802e9f6d27d81a
Bestun Gardi <bestun95@yahoo.com>
2018-08-31 08:33:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function updateDriverWithKeyAndValue(driver,key, value){
 
 destructivelyUpdateDriverWithKeyAndValue(driver,key,value)
 {
-   Object.assign(driver,{[key]:value});
+   driver[key] = value;
    return driver
 }
